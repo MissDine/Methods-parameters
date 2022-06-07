6adcc4c5f8e88d1a3ed61acf888e8e4c55ff550c
MissDine <enidnyatichi1212@gmail.com>
2022-06-07 08:49:53 +0300

    Feature edit

---
+++ app.rb
@@ -0,0 +1,20 @@
+# Methods are tiny programmes which you can execute to do particular events.-Equivalent of a function in js
+def number
+    2
+end
+number    
+puts number + number
+# Parameters-
+def add num1
+    puts num1
+    num1
+end
+puts add(3) + add(4)  
+# the above method prints both numbers and their sum 
+def mult no1, no2
+    puts no1
+    puts no2
+    no1 * no2
+end
+
+puts mult(5,2) * mult(4,5)
